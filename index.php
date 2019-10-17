361cd54f4d8e1741b035cc6592e7e832ce7371ce
Charles Roper <charles@roper.im>
2019-10-17 18:18:34 +0100

    Add intructions disclosure

--- index.php
+++ index.php
@@ -105,12 +105,42 @@ $imd_data = $db->query(
 <header>
 	<h1>English IMD Postcode Checker</h1>
 </header>
-<!-- <details>
-<summary>What is this?</summary>
-The Indices of Multiple Deprivation.
-</details> -->
 
 <main>
+
+<details>
+	<summary><h2>What is this?</h2></summary>
+	<p>This tool enables you to look up the Index of Multiple Deprivation rank for a list of postcodes. The lower the rank, the more deprived the area.</p>
+
+	<p>The results can be limited to a maximum decile value. A <i>decile</i> is a range divided into 10 chunks similar to the way a percentage is a range divided into 100 chunks. A decile of 1 means the postcode is in the bottom 10% of of the deprivation index, a decile of 2 means the postcode is in the bottom 20%, and so on.</p>
+
+	<h3>What is the IMD?</h2>
+
+	<p>The Index of Multiple Deprivation, commonly known as the IMD, is the official measure of relative deprivation for small areas in England.</p>
+
+	<p>The Index of Multiple Deprivation ranks every small area, called lower-layer super output areas (LSOA), in
+	England from 1 (most deprived area) to 32,844 (least deprived area).</p>
+
+	<p>The IMD combines information from the seven domains to produce an overall relative measure of deprivation. The domains are combined using the following weights:</p>
+
+	<ul>
+		<li>Income Deprivation (22.5%)</li>
+		<li>Employment Deprivation (22.5%)</li>
+		<li>Education, Skills and Training Deprivation (13.5%)</li>
+		<li>Health Deprivation and Disability (13.5%)</li>
+		<li>Crime (9.3%)</li>
+		<li>Barriers to Housing and Services (9.3%)</li>
+		<li>Living Environment Deprivation (9.3%)</li>
+	</ul>
+
+	<h2>Data used in this tool</h2>
+
+	<ul>
+		<li><a href="https://www.ons.gov.uk/methodology/geography/geographicalproducts/postcodeproducts">ONS Postcode Directory (ONSPD)</a></li>
+		<li><a href="https://www.gov.uk/government/statistics/english-indices-of-deprivation-2019">English Index of Multiple Deprivation 2019 (IMD)</a></li>
+	</ul>
+</details>
+
 <form action="./index.php" method="get" class="flow">
 	<label for="postcodes">
 		Enter Postcodes<br>
