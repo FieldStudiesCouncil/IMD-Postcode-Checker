d72fc6bb52ae69551aee2ce4cc97dee4ec21af58
Charles Roper <charles@roper.im>
2020-12-04 22:53:54 +0000

    Add meta description. Closes #2

--- index.php
+++ index.php
@@ -112,6 +112,7 @@ $imd_data = $db->query(
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<meta name="description" content="This simple tool enables you to look up the Index of Multiple Deprivation (IMD) rank for a list of postcodes."
 	<title>IMD Postcode Checker</title>
 	<link rel="stylesheet" href="./style.css?v=4">
 	</head>
